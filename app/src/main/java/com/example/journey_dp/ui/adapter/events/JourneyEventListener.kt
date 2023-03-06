710966da57e57eafd9a08adad46743131e291a32
luxao <lukas.lobll@gmail.com>
2023-03-06 21:37:02 +0100

    Rename one class

--- app/src/main/java/com/example/journey_dp/ui/adapter/events/JourneyEventListener.kt
+++ app/src/main/java/com/example/journey_dp/ui/adapter/events/JourneyEventListener.kt
@@ -1,10 +1,9 @@
 package com.example.journey_dp.ui.adapter.events
 
 
-import androidx.navigation.fragment.findNavController
 import com.google.android.material.textfield.TextInputEditText
 
-class DestinationEventListener(val clickListener: (input: TextInputEditText?) -> Unit) {
+class JourneyEventListener(val clickListener: (input: TextInputEditText?) -> Unit) {
     fun onClick(input: TextInputEditText) = clickListener(input)
 }
 
