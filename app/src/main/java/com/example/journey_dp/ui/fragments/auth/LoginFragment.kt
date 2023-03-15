db0b9077ec865a05a622836569aabfc4121cbd5e
luxao <lukas.lobll@gmail.com>
2023-03-15 19:13:19 +0100

    Remove deprecated method and change it to new method in google login

--- app/src/main/java/com/example/journey_dp/ui/fragments/auth/LoginFragment.kt
+++ app/src/main/java/com/example/journey_dp/ui/fragments/auth/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.example.journey_dp.ui.fragments.auth
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.findNavController
 import com.example.journey_dp.R
 import com.example.journey_dp.databinding.FragmentLoginBinding
@@ -31,6 +33,24 @@ class LoginFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
 
+    private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        Log.i(TAG, "ResultCODE = ${result.resultCode}")
+        when (result.resultCode) {
+            Activity.RESULT_OK -> {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                if (task.isSuccessful) {
+                    val account = task.getResult(ApiException::class.java)!!
+                    firebaseAuthWithGoogle(account.idToken!!)
+                }
+                else {
+                    Log.w(TAG, "Google sign in failed")
+                }
+            }
+            Activity.RESULT_CANCELED -> {
+                Log.w(TAG, "Google sign was canceled")
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,7 +84,7 @@ class LoginFragment : Fragment() {
 
     private fun signIn() {
         val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
+        resultLauncher.launch(signInIntent)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -75,21 +95,23 @@ class LoginFragment : Fragment() {
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                // Google Sign-In was successful, authenticate with Firebase
-                val account = task.getResult(ApiException::class.java)!!
-                firebaseAuthWithGoogle(account.idToken!!)
-            } catch (e: ApiException) {
-                // Google Sign-In failed, update UI accordingly
-                Log.w(TAG, "Google sign in failed", e)
-            }
-        }
-    }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        if (requestCode == RC_SIGN_IN) {
+//            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+//            try {
+//                // Google Sign-In was successful, authenticate with Firebase
+//                val account = task.getResult(ApiException::class.java)!!
+//                firebaseAuthWithGoogle(account.idToken!!)
+//            } catch (e: ApiException) {
+//                // Google Sign-In failed, update UI accordingly
+//                Log.w(TAG, "Google sign in failed", e)
+//            }
+//        }
+//    }
 
     private fun firebaseAuthWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
